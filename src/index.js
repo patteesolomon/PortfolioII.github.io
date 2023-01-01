9cf0f7994b22d055ea3ddaacff1609ac622e4109
patteesolomon <patteesolomon@gmail.com>
2023-01-01 02:35:38 -0600

    deploy main agg

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import Main from './views/Main';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React>
     <Main/>
-    </React.StrictMode>
+    </React>
 );
